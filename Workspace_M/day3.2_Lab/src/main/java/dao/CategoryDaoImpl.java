86b1f1f205d1fb19ee6844a3c06e43a0a7276a52
faraz2192 <faraz2192@gmail.com>
2023-01-25 16:08:17 +0530

    day3.2_labwork

---
+++ Workspace_M/day3.2_Lab/src/main/java/dao/CategoryDaoImpl.java
@@ -0,0 +1,58 @@
+package dao;
+
+import static utils.HibernateUtils.getFactory;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import pojos.Category;
+
+public class CategoryDaoImpl implements CategoryDao {
+
+	@Override
+	public String addNewCategory(Category newCategory) {
+		String mesg = "Adding category failed";
+		// get session from SF
+		Session session = getFactory().getCurrentSession();
+		// begin a Tx
+		Transaction tx = session.beginTransaction();
+		try {
+//			Transient to persistent
+			session.persist(newCategory);
+//			We will have to individually iterate through all products and
+			tx.commit();
+			mesg = "New category added with ID " + newCategory.getId();
+		} catch (RuntimeException e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		}
+		return mesg;
+	}
+
+	@Override
+	public String deleteCategoryName(Category categoryName) {
+		String jpql="select c from Category c where c.categoryName=:nm";
+
+		//		Delete query will not work here because the it will fail at
+//		Database Level;
+		
+		String message="Deleting Category Failed";
+//		Getting session from Session Factory;
+		Session session = getFactory().getCurrentSession();
+		Transaction tx= session.beginTransaction();
+		try {
+			int updateCount = session.createNamedQuery(jpql).
+					setParameter("nm", categoryName).executeUpdate();
+			tx.commit();
+			message="Deletion of Category successfull"+updateCount;
+		} catch (RuntimeException e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		}
+		return message;
+		
+	}
+
+}
